0b3a7149f8d6c0bfbddc2a16ee45cde250420b40
husamrio <zulfiqrd@gmail.com>
2024-01-03 22:58:00 +0300

    100-await.js

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,13 @@
+/* Await/Async */
+import { uploadPhoto, createUser } from './utils';
+
+export default async () => {
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+
+    return Promise.resolve({ photo, user });
+  } catch (err) {
+    return Promise.resolve({ photo: null, user: null });
+  }
+};
