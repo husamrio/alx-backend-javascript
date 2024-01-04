d9fee69b5ddda32e052cccf9437dd498abe145dc
husamrio <zulfiqrd@gmail.com>
2024-01-04 22:18:10 +0300

    6-sky_high

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,17 @@
+/* Import building from 5-bulding.js */
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
