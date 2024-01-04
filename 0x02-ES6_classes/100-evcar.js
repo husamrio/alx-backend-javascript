0dbc5b09cba170d98bbceb3c90b300744beaa0d7
husamrio <zulfiqrd@gmail.com>
2024-01-04 22:50:18 +0300

    100-evcar

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,16 @@
+/**
+ * eslint-disable
+ * import Car from 10-car.js
+ */
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  cloneCar() {
+    return new Car();
+  }
+}
