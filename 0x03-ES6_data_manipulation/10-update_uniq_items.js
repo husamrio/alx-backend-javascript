862e80823cde1dd2294ecf989dff58e72bdbc6b6
husamrio <zulfiqrd@gmail.com>
2024-01-09 16:51:49 +0300

    10-update_uniq_items.js

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,14 @@
+/**
+ * Alters  the no of unique grocery items to 100.
+ * @param Map<String, number> map
+ */
+export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+  map.forEach((value, key) => {
+    if (value === 1) {
+      map.set(key, 100);
+    }
+  });
+}
