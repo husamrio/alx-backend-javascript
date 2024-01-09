b1bac0d1fb87c39f3969601bd68e7f6914f54465
husamrio <zulfiqrd@gmail.com>
2024-01-09 16:44:54 +0300

    9-groceries_list.js

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,12 @@
+/**
+ * Fetches a map of grocery names and their quantity.
+ * @returns Maps<string number
+ */
+const groceriesList = () => new Map()
+  .set('Apples', 10)
+  .set('Tomatoes', 10)
+  .set('Pasta', 1)
+  .set('Rice', 1)
+  .set('Banana', 5);
+
+export default groceriesList;
