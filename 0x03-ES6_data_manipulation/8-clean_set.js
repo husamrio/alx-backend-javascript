48952d5c135d34864fcbb23dd0a5953d36a579ee
husamrio <zulfiqrd@gmail.com>
2024-01-09 16:39:44 +0300

    8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,22 @@
-/* Combines a set of strings with a dash after stripping the leading sub string */
+/**
+ * Joins a set of strings with a dash after stripping the strings
+ * @param {Set<String>} set - A collection of strings.
+ * @author Hussein Abdullahi <https://github.com/husamrio>
+ * @returns {String}
+ */
 export default function cleanSet(set, startString) {
-  if (!startString || typeof startString !== 'string' || typeof set !== 'object') return '';
-  return [...set]
-    .filter((el) => el.startsWith(startString))
-    .map((el) => el.slice(startString.length))
-    .join('-');
+  const parts = [];
+  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
+    return '';
+  }
+  for (const value of set.values()) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      const valueSubStr = value.substring(startString.length);
+
+      if (valueSubStr && valueSubStr !== value) {
+        parts.push(valueSubStr);
+      }
+    }
+  }
+  return parts.join('-');
 }
