95df53c5d7a8aac1b2ccaf2027d019dee8a92b9c
husamrio <zulfiqrd@gmail.com>
2024-01-09 14:44:00 +0300

    4-update_grade_by_city.js

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,24 @@
+/**
+ * Updates list of students in a given city
+ * @param {id: Number} students 
+ * @param {*firstName: String} city 
+ * @param {*location: String} newGrades 
+ * @returns 
+ */
+export default function updateStudentGradeByCity(students, city, newGrades) {
+    if (!Array.isArray(students)) {
+      return [];
+    }
+    if (!Array.isArray(newGrades)) {
+      return [];
+    }
+    return students
+      .filter((student) => student.location === city)
+      .map((student) => {
+        const [newGrade] = newGrades.filter((item) => item.studentId === student.id);
+        return {
+          ...student,
+          grade: newGrade ? newGrade.grade : 'N/A',
+        };
+      });
+}
