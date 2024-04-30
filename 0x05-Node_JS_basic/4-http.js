399e895d6f903eb94ad68fc823cf30d7415ee5be
husamrio <zulfiqrd@gmail.com>
2024-04-30 12:52:33 +0300

    Create a small HTTP server using Node HTTP module

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,10 +1,12 @@
 const http = require('http');
 
-// find and get
+// find and get the prog
+
 const hostname = '127.0.0.1';
 const port = 1245;
 
-// Regex integration testing
+// Regex integration testing the prog
+
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
